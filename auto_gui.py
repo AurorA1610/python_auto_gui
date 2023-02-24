c496a2574d601676fdf6ba59d5d2fe0c1bb1e5bb
Aurora Lithe Roy <86685618+AurorA1610@users.noreply.github.com>
2023-02-24 16:00:16 +0530

    Update auto_gui.py

--- auto_gui.py
+++ auto_gui.py
@@ -3,7 +3,7 @@ import time
 no_of_messages = 100
 while no_of_messages > 0:
     time.sleep(4)
-    pyautogui.typewrite("Hi. I love you. Bye.")
+    pyautogui.typewrite("Hi. How are you?")
     time.sleep(2)
     pyautogui.press("enter")
     no_of_messages = no_of_messages - 1
